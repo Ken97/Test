9d4d5b29bcd6cd118cc07520e113e8a0235d98cc
ken97 <kenjanssen1997@gmail.com>
2015-12-17 15:11:04 +0100

    som

---
+++ som.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int a, b, c;
+int som;
+
+int main(void)
+{
+    printf("geef 3 getallen\n");
+    printf("geef een eerste getal:\n");
+    scanf("%d", &a);
+    printf("geef een tweede getal:\n");
+    scanf("%d", &b);
+    printf("geef een derde getal:\n");
+    scanf("%d", &c);
+    som = (a + b + c);
+            printf("de som van de 3 getallen is %d\n", som);
+    return 0;
+}
